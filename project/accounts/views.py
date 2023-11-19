0a42006d13a7b2f590ce385de6e566fc1403e935
AImaltsev <boomer47.am@gmail.com>
2023-11-19 15:54:12 +0400

    Регистрация работает, но пока без подтверждения

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-
+from .forms import CustomUserCreationForm
+from django.contrib import messages
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm  # Замените на новую форму
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'There was an error with your registration. Please check the form and try again.')
+        return super().form_invalid(form)
